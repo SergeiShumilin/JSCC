7515a5794db545c98d68087b4adf3a8ad0fe91c8
Sergei <NoiSD@yandex.ru>
2018-05-21 12:02:14 +0300

    Add files via upload

--- FirstTask.py
+++ FirstTask.py
@@ -1,6 +1,9 @@
+from __future__ import unicode_literals
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from numpy import *
 import matplotlib.pyplot as plt
+from matplotlib import rc
+
 import seaborn
 
 fig = plt.figure()
@@ -15,9 +18,13 @@ xax.set_major_locator(majorLocator)
 xax.set_major_formatter(majorFormatter)
 
 # Различные преобразования с массивом labels
-for label in xax.get_ticklabels():
-    label.set_fontsize(10)
 
+tL = ['0',r'$\frac{1}{v}$',r'$\frac{2}{v}$',r'$\frac{3}{v}$',r'$\frac{4}{v}$',r'$\frac{5}{v}$',r'$\frac{6}{v}$',r'$\frac{7}{v}$',r'$\frac{8}{v}$',
+      r'$\frac{9}{v}$', r'$\frac{10}{v}$', r'$\frac{11}{v}$', r'$\frac{12}{v}$', r'$\frac{13}{v}$', r'$\frac{14}{v}$',
+      r'$\frac{15}{v}$', r'$1']
+xax.set_ticklabels(tL)
+for label in xax.get_ticklabels():
+    label.set_fontsize(14)
 # Лист значений параметра q
 q = arange(1 / 16, 1, 1 / 16)
 
@@ -34,15 +41,15 @@ def plotAFunction(q):
     k = 0
     alpha = 0.2
     for c in q:
-        ax.axvline(x=c, linestyle="dashed", dashes=(20, 8), linewidth=1, color=colors[i])  # ассимптота
+        ax.axvline(x=c, linestyle="dashed", dashes=(20, 8), linewidth=0.5, color='black')  # ассимптота
         x = arange(0, c, 0.001)
-        ax.plot(x, 1 + ((1 - 2 * c) / (c - x)), color=colors[i], linewidth=2)  # сам график
+        ax.plot(x, 1 + ((1 - 2 * c) / (c - x)), color='black', linewidth=1)  # сам график
         k += 1
         if k == 4:
-            ax.axvline(x=c, linestyle="dashed", dashes=(20, 8), linewidth=2, color=colors[i])
-            ax.plot(x, 1 + ((1 - 2 * c) / (c - x)), color=colors[i], linewidth=4)  # сам график
+            ax.axvline(x=c, linestyle="dashed", dashes=(20, 8), linewidth=0.5, color='black')
+            ax.plot(x, 1 + ((1 - 2 * c) / (c - x)), color='black', linewidth=1.5)  # сам график
 
-            x = arange(0, 0.5, 0.001)
+            x = arange(0, 0.250, 0.001)
             ax.fill_between(x, 1 + ((1 - 2 * c) / (c - x)), 0, facecolors='green', alpha=alpha)
             x = arange(0.249, 0.5, 0.001)
             ax.fill_between(x, 25, 0, facecolors='green', alpha=alpha, label='using vectorization is beneficial')
@@ -56,20 +63,19 @@ def plotAFunction(q):
 
     # Размеры графика
     plt.ylim(0, 25)
-    plt.xlim(0, 0.45)
+    plt.xlim(0, 0.5)
 
 
 # текст
-plt.text(0.17, 13.0, 'k = 4', size=15, rotation=73.)
+plt.text(0.17, 13.0, r'$k = 4$', size=15, rotation=73.)
 
 # Тектовое и графическое оформление графика
-plt.title("Graphic's name", fontsize=13)
-plt.xlabel('X values', fontsize=11)
-plt.ylabel('Y values', fontsize=11)
+plt.title("Evaluation of vectorizing efficiency", fontsize=13)
+plt.xlabel(r'$p_2$', fontsize=15)
+plt.ylabel(r'$\frac{t_2}{t_1}$', fontsize=20,rotation = 0)
 ax.set_facecolor('w')  ##f0f5f5
 
 ax.yaxis.grid(True)
 
 plotAFunction(q)
 plt.show()
-
