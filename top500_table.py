9cb5795a1709c28fbfcb3088afb516feab55b71e
Sergei <NoiSD@yandex.ru>
2018-06-22 19:12:05 +0300

    The pandas based top500 SCs table
    The python module which interacts with the TOP500 supercomputers table published on the https://www.top500.org/list/2017/11/
    And allows to retrieve some information from it using some pandas features

---
+++ top500_table.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import urllib
+import html5lib
+import bs4
+
+myurl100 = urllib.request.urlopen('https://www.top500.org/list/2017/11/?page=1')
+myurl200 = urllib.request.urlopen('https://www.top500.org/list/2017/11/?page=2')
+myurl300 = urllib.request.urlopen('https://www.top500.org/list/2017/11/?page=3')
+myurl400 = urllib.request.urlopen('https://www.top500.org/list/2017/11/?page=4')
+myurl500 = urllib.request.urlopen('https://www.top500.org/list/2017/11/?page=5')
+top500_table100 = pd.read_html(myurl100, flavor='bs4')
+top500_table200 = pd.read_html(myurl200, flavor='bs4')
+top500_table300 = pd.read_html(myurl300, flavor='bs4')
+top500_table400 = pd.read_html(myurl400, flavor='bs4')
+top500_table500 = pd.read_html(myurl500, flavor='bs4')
+top100 = pd.concat(top500_table100)
+top200 = pd.concat(top500_table200)
+top300 = pd.concat(top500_table300)
+top400 = pd.concat(top500_table400)
+top500 = pd.concat(top500_table500)
+top_500_full = top100.append(top200, ignore_index=True). \
+    append(top300, ignore_index=True). \
+    append(top400, ignore_index=True). \
+    append(top500, ignore_index=True)
+top_500_full.set_index(['Rank'], inplace=True)
+smth = top_500_full[top_500_full['System'].str.contains('Phi', na = False)]
+print(smth)
+print('----------------------------------------------------------------')
+#print((smth==True).sum())
+# top_500_full.to_csv('top500.csv',sep=';')
