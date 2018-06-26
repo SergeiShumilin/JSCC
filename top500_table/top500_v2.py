598ace4ede6893bb645dfc9df20a8512a43c4c0e
Sergei <NoiSD@yandex.ru>
2018-06-26 19:34:40 +0300

    The code structure was refined

--- top500_table/top500_v2.py
+++ top500_table/top500_v2.py
@@ -1,56 +1,93 @@
+# Author: Sergei Shumilin
+# Organisation: Joint Super Computer Center of the Russian Academy of Sciences
+# Copyright: This module has been placed in the public domain.
+
+"""
+This module retrieves information from the top500 supercomputers (SC) table
+from top500.org and defines the following indicators:
+
+a) The total peak performance (Rpeak [TFlop/s]) of the SC in a given country;
+
+б) The ratio of peak performance of SCs based on accelerators to the entire
+performance of all SC in the given country.
+
+How To Use This Module
+======================
+(See the individual classes, methods, and attributes for details.)
+
+1. Download the TOP500 exel file on your computer.
+
+2. Convert it to the .csv format (Use comma separation, pay attention to encoding)
+
+3. In the get_the_full_table() function define the path to the .csv table.
+
+4. Use get_the_countrys_total_peak_perfomance(region) function to get the total peak performance (Rpeak [TFlop/s]) of
+the SCs in the given country.
+    The countries you can get the info about (The :arg region must be exactly similar as shown down:
+        a) 'China'
+        b) 'United States'
+        c) 'EU' (European Union)
+        d) 'Russia'
+        e) 'Others'
+
+5. Use get_accelerators_to_entire_performance_ratio(region) function to get the ratio of peak performance of SCs based
+ on accelerators to the entire performance of all SCs in the given country.
+    The countries you can get the info about (The :arg region must be exactly similar as shown down:
+        a) 'China'
+        b) 'United States'
+        c) 'EU' (European Union)
+        d) 'Russia'
+        e) 'Others'
+
+"""
+
+__docformat__ = 'restructuredtext'
+
 import pandas as pd
 
 
-def get_the_csv_notnull_table():
-    top500_table = pd.read_csv('TOP500_201806.csv', sep=',')
-    top500_table.set_index(['Rank'], inplace=True)
-    # print('The total peak performance is ' + str(top500_table['Rpeak [TFlop/s]'].sum()))
-    table_with_notnull_values = top500_table[top500_table['Accelerator/Co-Processor Cores'].notnull()]
-    return table_with_notnull_values
+def make_notnull_table(full_table):
+    table_without_notnull_values = full_table[full_table['Accelerator/Co-Processor Cores'].notnull()]
+    return table_without_notnull_values
 
-def get_the_csv_full_table():
+def get_the_full_table():
     top500_table = pd.read_csv('TOP500_201806.csv', sep=',')
     top500_table.set_index(['Rank'], inplace=True)
-    print('The total peak performance is ' + str(top500_table['Rpeak [TFlop/s]'].sum()))
     return top500_table
 
-def get_the_ratio1(country):
-    country_table = get_the_csv_notnull_table[get_the_csv_notnull_table['Country'] == country]
-    summary_peak_performance = get_the_csv_notnull_table['Rpeak [TFlop/s]'].sum()
-    summary__country_peak_performance = country_table['Rpeak [TFlop/s]'].sum()
-    return 'The relation of peak performance to the summary world one of '+country+' is ' + str(summary__country_peak_performance/summary_peak_performance)
-
-def get_the_ratio2(country):
-    table = get_the_csv_full_table()
-    country_table = table[table['Country'] == country]
-    summary_peak_performance = country_table['Rpeak [TFlop/s]'].sum()
-    country_table = country_table[country_table['Accelerator/Co-Processor Cores'].notnull()]
-    summary_country_peak_performance = country_table['Rpeak [TFlop/s]'].sum()
-    return "The relation of peak performance to the country's summary one of " + country + ' is ' + str(
-        summary_country_peak_performance / summary_peak_performance)
-
-
+def get_total_peak_performance():
+    table = get_the_full_table()
+    return 'The total peak performance is ' + str(table['Rpeak [TFlop/s]'].sum())
 
 def get_euro_countries_table():
-    table = get_the_csv_full_table()
+    table = get_the_full_table()
     euro_countries_table = table[(table['Region']=='Northern Europe') |
                                  (table['Region']=='Western Europe')|
                                  (table['Region']=='Southern Europe')|
-                                 (table['Region']=='Eastern Europe')& (table['Country']!='Russia')]
+                                 (table['Region']=='Eastern Europe')& (table['Country']!='Russia')
+                                 & (table['Country'] != 'Switzerland')]
     return euro_countries_table
 
-
-def get_euro_countries_ratio(region,table):
-    table = table
+def get_accelerators_to_entire_performance_ratio(region):
+    table = get_region_table(region)
     summary_peak_performance = table['Rpeak [TFlop/s]'].sum()
-    table = table[table['Accelerator/Co-Processor Cores'].notnull()]
+    table = make_notnull_table(table)
     summary_country_peak_performance = table['Rpeak [TFlop/s]'].sum()
-    return "The relation of peak performance to the country's summary one of" +region + "is " + str(
+    return "The ratio of peak performance of SCs based on accelerators to the " + region + \
+           "'s summary peak performance" + " is " + str(
         summary_country_peak_performance / summary_peak_performance)
 
+def get_region_table(region):
+    if region == 'EU':
+        table = get_euro_countries_table()
+    elif region == 'Others':
+        table = get_other_countries_table()
+    else:
+        table = get_the_countrys_table(region)
+    return table
 
 def get_other_countries_table():
-    table = get_the_csv_full_table()
+    table = get_the_full_table()
     other_countries_table = table[(table['Region']!='Northern Europe')&
                                  (table['Region']!='Western Europe')&
                                  (table['Region']!='Southern Europe')&
@@ -60,8 +97,12 @@ def get_other_countries_table():
                                   & (table['Country']!='China')]
     return other_countries_table
 
-print(get_the_ratio2('China'))
-print(get_the_ratio2('United States'))
-print(get_the_ratio2('Russia'))
-print(get_euro_countries_ratio('European Union',get_euro_countries_table()))
-print(get_euro_countries_ratio('other countries',get_other_countries_table()))
+def get_the_countrys_table(country):
+    table = get_the_full_table()
+    country_table = table[table['Country'] == country]
+    return country_table
+
+def get_the_countrys_total_peak_perfomance(region):
+    region_table = get_region_table(region)
+    summary__country_peak_performance = region_table['Rpeak [TFlop/s]'].sum()
+    return 'The total performance of ' + region + ' is ' + str(summary__country_peak_performance)
